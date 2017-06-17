009e070cf0896e069734cb2c430104be2e6ae2c5
Diego Dulval <diegodifreitas@gmail.com>
2017-06-16 21:34:14 -0300

    Novos testes

---
+++ src/Comment.test.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { shallow, mount, render } from 'enzyme'
+import Comment from './Comment'
+
+describe('<Comment />', () => {
+    it('renders without crashing', () => {
+        const comment = { comment: 'teste' }
+        const wrapper = shallow(<Comment comment={comment} />)
+        expect(wrapper.length).toBe(1)
+        expect(wrapper.text()).toBe(comment.comment)
+         expect(wrapper.is('.well')).toBe(true)
+    })
+})
